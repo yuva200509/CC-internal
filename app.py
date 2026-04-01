4b4126ae5279ddf0e880c195ec18018780243950
k yuva sai <160228640+yuva200509@users.noreply.github.com>
2026-04-01 08:47:02 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+flask.py from flask import Flask 
+app = Flask(__name__) 
+@app.route("/") 
+def home(): 
+return "Flask Web Services"
+if __name__ == "__main__": 
+app.run(host="0.0.0.0", port=5000)
